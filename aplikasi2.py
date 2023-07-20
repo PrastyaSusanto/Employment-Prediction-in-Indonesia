e26e44ccf44b4245a804a42eefffb2ba2a9199f3
Prastya Susanto <134188611+PrastyaSusanto@users.noreply.github.com>
2023-07-20 10:35:55 +0700

    Update aplikasi2.py

--- aplikasi2.py
+++ aplikasi2.py
@@ -16,74 +16,53 @@ def main():
     st.write("For more information about this project, check here: [GitHub Repo](https://github.com/PrastyaSusanto/Commuter-Prediction-App/tree/main)")
 
     form = st.form("Data Input")
-    options = ['Employed', 'Unemployed']
-    selected_options = form.multiselect('Employed/Unemployed', options, default=options)
+    Option = form.selectbox('Employed/Unemployed', ['Employed', 'Unemployed'])
     start_date = form.date_input('Start Date')
     end_date = form.date_input('End Date')
 
     submit = form.form_submit_button("Predict")  # Add a submit button
 
     if submit:
-        # Create a list to store the selected 'Option' for each date
-        option_list = []
-        for date in pd.date_range(start=start_date, end=end_date):
-            option_list.extend(selected_options)
-
         data = {
-            'Tanggal Referensi': pd.date_range(start=start_date, end=end_date).repeat(len(selected_options)),
-            'Option': [0 if option == 'Employed' else 1 for option in option_list]
+            'Kode Tipe': Option,
+            'Tanggal Referensi': pd.date_range(start=start_date, end=end_date).to_list()
         }
         data = pd.DataFrame(data)
 
+        data['Kode Tipe'] = data['Kode Wilayah'].replace({'Employed': 0, 'Unemployed': 1})
+
         # Convert Tanggal column to datetime and calculate the difference from the reference date
-        data['Tanggal Referensi'] = (data['Tanggal Referensi'] - pd.to_datetime('2011-02-01')).dt.days
+        data['Tanggal Referensi'] = (pd.to_datetime(data['Tanggal Referensi']) - pd.to_datetime('2011-02-01')).dt.days
 
         # Load the model from the pickle file
         with open('model.pkl', 'rb') as f:
             model = pickle.load(f)
-
-        # Use PolynomialFeatures with degree 2 to transform input data
-        poly = PolynomialFeatures(degree=2)
-        data_poly = poly.fit_transform(data[['Tanggal Referensi', 'Option']])
-
-        # Make prediction using the loaded model and transformed data_poly
-        predictions = model.predict(data_poly)
+        
+        # Make prediction using the loaded model
+        predictions = model.predict(data)
 
         # Create a DataFrame to store the results
-        results = pd.DataFrame({'Date': pd.date_range(start=start_date, end=end_date).repeat(len(selected_options)), 'Option': option_list, 'Predicted Number': predictions})
+        results = pd.DataFrame({'Date': pd.date_range(start=start_date, end=end_date), 'Predicted Number': predictions})
 
-        # Format the Date column in the results DataFrame and remove time part
+        # Format the Date column in the results DataFrame
         results['Date'] = results['Date'].dt.strftime('%d-%m-%Y')
 
-        # If only one option is selected, adjust the column name accordingly
-        if len(selected_options) == 1:
-            option_name = selected_options[0]
-            pivot_results = results.pivot(index='Date', columns='Option', values='Predicted Number')
-            pivot_results.columns = [option_name]
-            plt.plot(pivot_results.index, pivot_results[option_name], color='royalblue')
-        else:
-            # Pivot the DataFrame to create separate columns for Employed and Unemployed
-            pivot_results = results.pivot(index='Date', columns='Option', values='Predicted Number')
-            pivot_results.columns = ['Employed', 'Unemployed']
-            plt.plot(pivot_results.index, pivot_results['Employed'], color='royalblue')
-            plt.plot(pivot_results.index, pivot_results['Unemployed'], color='white')
-
-        # Visualize the results using one or two line charts
+        # Visualize the results using matplotlib
+        plt.style.use('dark_background') 
+        plt.plot(results['Date'], results['Predicted Number'], color='royalblue')
         plt.xlabel('Date')
         plt.ylabel('Predicted Number')
         plt.xticks(rotation=90)
         plt.gca().spines['top'].set_visible(False)
         plt.gca().spines['right'].set_visible(False)
-        if len(selected_options) == 1:
-            plt.title('Predicted Amount of {} over Time'.format(option_name))
-        else:
-            plt.title('Predicted Amount of Employed and Unemployed over Time')
+        plt.title('Predicted Amount of '+Option+" over Time")
         plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=10))  # Set maximum number of x-axis ticks
-
-        st.pyplot()
+        
+        st.pyplot(plt)
 
         # Show the table with three columns: Date, Employed, and Unemployed
-        st.dataframe(pivot_results)
+        st.dataframe(results)
+    
 
 if __name__ == '__main__':
     main()

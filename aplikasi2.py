4c92a09586607360933d886fd0b2c0d62567a5b1
Prastya Susanto <134188611+PrastyaSusanto@users.noreply.github.com>
2023-07-20 10:16:08 +0700

    Update aplikasi2.py

--- aplikasi2.py
+++ aplikasi2.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from sklearn.preprocessing import PolynomialFeatures
 
 title = 'Application for predicting employed and unemployed individuals in Indonesia👷‍♀️👷🏢'
 subtitle = 'Predict the number of employed and unemployed individuals in Indonesia using Machine Learning👷‍♀️👷🏢 '
@@ -23,48 +24,49 @@ def main():
     submit = form.form_submit_button("Predict")  # Add a submit button
 
     if submit:
-        data = pd.DataFrame({'Tanggal Referensi': pd.date_range(start=start_date, end=end_date)})
-        data['Kode Tipe'] = data['Tanggal Referensi'].apply(lambda date: 1 if 'Unemployed' in selected_options else 0)
+        data = {
+            'Tanggal Referensi': pd.date_range(start=start_date, end=end_date),
+            'Option': selected_options * len(pd.date_range(start=start_date, end=end_date))
+        }
+        data = pd.DataFrame(data)
+
+        # Convert Tanggal column to datetime and calculate the difference from the reference date
         data['Tanggal Referensi'] = (data['Tanggal Referensi'] - pd.to_datetime('2011-02-01')).dt.days
 
         # Load the model from the pickle file
         with open('model.pkl', 'rb') as f:
             model = pickle.load(f)
-        
-        # Make prediction using the loaded model
-        predictions = model.predict(data)
+
+        # Use PolynomialFeatures with degree 2 to transform input data
+        poly = PolynomialFeatures(degree=2)
+        data_poly = poly.fit_transform(data[['Tanggal Referensi', 'Option']])
+
+        # Make prediction using the loaded model and transformed data_poly
+        predictions = model.predict(data_poly)
 
         # Create a DataFrame to store the results
-        results = pd.DataFrame({
-            'Date': pd.date_range(start=start_date, end=end_date),
-            'Employed': predictions[:, 0],  # Predicted values for 'Employed'
-            'Unemployed': predictions[:, 1]  # Predicted values for 'Unemployed'
-        })
+        results = pd.DataFrame({'Date': pd.date_range(start=start_date, end=end_date), 'Predicted Number': predictions})
 
-        # Format the predicted values as integers
-        results['Employed'] = results['Employed'].astype(int)
-        results['Unemployed'] = results['Unemployed'].astype(int)
+        # Format the predicted passenger values as integers
+        results['Predicted Number'] = results['Predicted Number'].astype(int)
 
         # Visualize the results using matplotlib
-        plt.style.use('dark_background') 
-        plt.plot(results['Date'], results['Employed'], label='Employed', color='royalblue')
-        plt.plot(results['Date'], results['Unemployed'], label='Unemployed', color='salmon')
+        plt.style.use('dark_background')
+        plt.plot(results['Date'], results['Predicted Number'], color='royalblue')
         plt.xlabel('Date')
         plt.ylabel('Predicted Number')
         plt.xticks(rotation=90)
         plt.gca().spines['top'].set_visible(False)
         plt.gca().spines['right'].set_visible(False)
-        plt.legend()
-        plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=10))  # Set maximum number of x-axis ticks
         plt.title('Predicted Amount of Employed and Unemployed over Time')
-        
+        plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=10))  # Set maximum number of x-axis ticks
+
         st.pyplot(plt)
 
         # Format the Date column in the results DataFrame
         results['Date'] = results['Date'].dt.strftime('%d-%m-%Y')
 
         st.dataframe(results)
-    
 
 if __name__ == '__main__':
     main()
